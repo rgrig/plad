53a214dd710b0178d66e1dd5c9ac708fd5f6b168
Radu Grigore <radugrigore@gmail.com>
2019-01-27 18:36:55 +0000

    somewhat better adding in Rust

--- code/add.rs
+++ code/add.rs
@@ -1,16 +1,15 @@
 use std::io;
-use std::io::BufRead;
+use std::io::Read;
 
 fn main() {
-    let stdin = io::stdin();
-
-    for line in stdin.lock().lines() {
-        let s = line.expect("A");
-        let mut sum = 0;
-        for w in s.split_whitespace() {
-            let x : i32 = w.parse().expect("B");
-            sum += x;
-        }
-        println!("{}", sum);
+    let mut sum = 0;
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)
+        .expect("I/O Error");
+    for w in input.split_whitespace() {
+        let n : i64 = w.parse()
+            .expect("Please type integers");
+        sum += n
     }
+    println!("{}", sum);
 }
