85d341baeebe65997f0967428adf05e9a1a327b4
Radu Grigore <radugrigore@gmail.com>
2020-01-19 15:08:29 +0000

    old changes

--- code/add.rs
+++ code/add.rs
@@ -1,15 +1,28 @@
 use std::io;
 use std::io::Read;
 
-fn main() {
-    let mut sum = 0;
+#[derive(Debug)]
+enum Error {
+    Io(io::Error),
+    Pie(std::num::ParseIntError),
+    Eof(),
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Self { Error::Io(e) }
+}
+impl From<std::num::ParseIntError> for Error {
+    fn from(e: std::num::ParseIntError) -> Self { Error::Pie(e) }
+}
+
+fn main() -> Result<(),Error> {
     let mut input = String::new();
-    io::stdin().read_to_string(&mut input)
-        .expect("I/O Error");
-    for w in input.split_whitespace() {
-        let n : i64 = w.parse()
-            .expect("Please type integers");
-        sum += n
-    }
-    println!("{}", sum);
+    io::stdin().read_to_string(&mut input)?;
+    let mut iter = input.split_whitespace();
+    let a = iter.next().ok_or(Error::Eof())?;
+    let b = iter.next().ok_or(Error::Eof())?;
+    let a : i64 = a.parse()?;
+    let b : i64 = b.parse()?;
+    println!("{}", a+b);
+    Ok(())
 }
